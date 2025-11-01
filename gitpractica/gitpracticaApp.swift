fd13bb7ec60b9b82e89539bb698b32d881adbe0d
angelvn <angel.villegas>
2025-10-31 18:20:08 -0600

    git

---
+++ gitpractica/gitpracticaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitpracticaApp.swift
+//  gitpractica
+//
+//  Created by WIN603 on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct gitpracticaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

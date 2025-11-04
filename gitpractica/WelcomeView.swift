9154c6f99f27ffa38063ae857f0aa14e3a4c11fa
AHH <alexander.hernandez@iest.edu.mx>
2025-11-03 18:14:55 -0600

    prueba

--- gitpractica/WelcomeView.swift
+++ gitpractica/WelcomeView.swift
@@ -1,18 +1,35 @@
-//
-//  WelcomeView.swift
-//  gitpractica
-//
-//  Created by WIN603 on 03/11/25.
-//
-
 import SwiftUI
 
 struct WelcomeView: View {
+    @AppStorage("myName") var savedName: String = ""
+    @AppStorage("myAge") var savedAge: Int = 0
+    @State var name: String = ""
+    @State var ageText: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TextField("Dime tu nombre:", text: $name)
+                .padding()
+            TextField("Introduce tu edad:", text: $ageText)
+                
+                
+            Button(action: {
+                savedName = self.name
+                if let edad = Int(ageText){
+                    savedAge = edad
+                }
+        
+            }, label: {
+                Text("Guardar").padding().background(Color.green)
+            }).padding()
+            
+            
+            Text("Bienvenido \(savedName)")
+        }
     }
 }
 
-#Preview {
-    WelcomeView()
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
 }

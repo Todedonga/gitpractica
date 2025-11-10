b6d3fdba13063fef5545b5c99c7cf4d56cbd34ab
AHH <alexander.hernandez@iest.edu.mx>
2025-11-10 17:50:45 -0600

    xd

--- gitpractica/CounterView.swift
+++ gitpractica/CounterView.swift
@@ -1,18 +1,52 @@
-//
-//  CounterView.swift
-//  gitpractica
-//
-//  Created by WIN603 on 03/11/25.
-//
-
 import SwiftUI
 
 struct CounterView: View {
+    
+    @ObservedObject var counter : Counter = Counter()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            Text("\(self.counter.number)")
+                .font(.title)
+                .bold()
+            
+            HStack{
+                Button(action: {
+                    self.counter.stop()
+                }){
+                    HStack{
+                        
+                        Image(systemName: "stop.fill").foregroundColor(.white)
+                        
+                        Text("Stop")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                        
+                    } .padding(.all)
+                }.background(Color.red)
+                
+                
+                Button(action: {
+                    self.counter.start()
+                }){
+                    HStack{
+                        
+                        Image(systemName: "play.fill").foregroundColor(.white)
+                        
+                        Text("Start")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                        
+                    } .padding(.all)
+                }.background(Color.green)
+            }
+        }
     }
 }
 
-#Preview {
-    CounterView()
+struct CounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CounterView()
+    }
 }
